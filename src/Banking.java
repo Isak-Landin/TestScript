842c2e0434256cc6b0e7c1718c15d4fa177b7ede
Isak-Landin <utbisalan@gmail.com>
2023-08-19 14:10:45 +0200

    Update Banking.java

--- src/Banking.java
+++ src/Banking.java
@@ -3,6 +3,7 @@ import org.dreambot.api.methods.container.impl.Inventory;
 public class Banking extends Node{
     @Override
     public boolean validate(){
+        boolean toReturn = true;
         return Inventory.isFull();
     }
 
