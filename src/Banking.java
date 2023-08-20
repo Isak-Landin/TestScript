1fc31b1bd60d5f91f76df971f9455e6b85617bbd
Isak-Landin <utbisalan@gmail.com>
2023-08-21 00:44:35 +0200

    Update Banking.java

--- src/Banking.java
+++ src/Banking.java
@@ -1,14 +1,52 @@
 import org.dreambot.api.methods.container.impl.Inventory;
+import org.dreambot.api.methods.container.impl.bank.Bank;
+import org.dreambot.api.methods.container.impl.bank.BankLocation;
+import org.dreambot.api.methods.map.Tile;
+import org.dreambot.api.methods.interactive.Players;
+import org.dreambot.api.utilities.Sleep;
 
 public class Banking extends Node{
+    private static final int WALKING_ANIMATION = 1146; // Replace with the correct animation ID for walking
+    private static final int RUNNING_ANIMATION = 11499; // Replace with the correct animation ID for running
+
+    private static final int[] ITEMS_FOR_SHRIMPS = 
+
     @Override
     public boolean validate(){
-        boolean toReturn = true;
         return Inventory.isFull();
     }
 
     @Override
     public void execute(){
+        BankLocation nearest_bank = BankLocation.getNearest();
+        Tile bankCenterTile = nearest_bank.getCenter();
+        double maximumAllowedDistanceBeforeBanking = 5.0;
 
+        boolean hasReachedBank = MovingUtility.GoToLocation(maximumAllowedDistanceBeforeBanking, bankCenterTile);
+
+        while (!isPlayerInBankingState()) {
+            Bank.open();
+            Sleep.sleep(1000, 2000);
+        }
+
+        if (Bank.isOpen()) {
+            Sleep.sleep(500, 1200);
+            depositItems();
+        }
+
+    }
+
+    private void depositItems(int[] itemsToKeepInInventory){
+        if (Bank.isOpen()){
+            /* pass an integer list to the deposit, to decide which items not to deposit */
+            Bank.depositAllExcept(itemsToKeepInInventory);
+        }
+    }
+
+    private boolean isPlayerInBankingState() {
+        int currentAnimation = Players.getLocal().getAnimation();
+        return currentAnimation == WALKING_ANIMATION ||
+                currentAnimation == RUNNING_ANIMATION ||
+                Bank.isOpen();
     }
 }
