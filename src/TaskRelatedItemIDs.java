ef5373da9c4bc9963fe74e54397b7d0cd3b4da16
Isak-Landin <utbisalan@gmail.com>
2023-08-21 05:30:11 +0200

    Create TaskRelatedItemIDs.java

---
+++ src/TaskRelatedItemIDs.java
@@ -0,0 +1,6 @@
+public class TaskRelatedItemIDs {
+
+    public static final int[] ShrimpTools = {1111, 1111, 1111};
+    public static final int[] SardineTools = {1111, 1111, 1111, 1111};
+    public static final int[] HerringTools = {1111, 1111, 1111, 1111};
+}
