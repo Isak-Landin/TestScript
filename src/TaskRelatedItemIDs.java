e2d82bef1d63de8dcf8d3348b5ef44146dccd896
Isak-Landin <utbisalan@gmail.com>
2023-08-23 00:57:27 +0200

    Update TaskRelatedItemIDs.java

--- src/TaskRelatedItemIDs.java
+++ src/TaskRelatedItemIDs.java
@@ -3,4 +3,6 @@ public class TaskRelatedItemIDs {
     public static final int[] ShrimpTools = {1111, 1111, 1111};
     public static final int[] SardineTools = {1111, 1111, 1111, 1111};
     public static final int[] HerringTools = {1111, 1111, 1111, 1111};
+    public static final int[] TroutTools = {1111, 1111, 1111, 1111};
+    public static final int[] SalmonsTools = {1111, 1111, 1111, 1111};
 }
