740b361448ee515e149989162871b2cf47effeb4
Isak-Landin <utbisalan@gmail.com>
2023-08-23 22:10:41 +0200

    Create AllowedDistanceForLocationData.java

---
+++ src/AllowedDistanceForLocationData.java
@@ -0,0 +1,9 @@
+public class AllowedDistanceForLocationData {
+    public static final double allowedDistanceFromShrimpTile = 15;
+    public static final double allowedDistanceFromSardineTile = 15;
+    public static final double allowedDistanceFromHerringTile = 15;
+    public static final double allowedDistanceFromTroutTile = 15;
+    public static final double allowedDistanceFromSalmonTile = 15;
+
+
+}
