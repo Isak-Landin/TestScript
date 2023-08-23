98af5cbbce16999cd4a08e9b9ed514f1f1b8f787
Isak-Landin <utbisalan@gmail.com>
2023-08-23 22:11:32 +0200

    Update LocationManager.java

--- src/LocationManager.java
+++ src/LocationManager.java
@@ -1,5 +1,7 @@
+import org.dreambot.api.methods.container.impl.bank.Bank;
 import org.dreambot.api.methods.container.impl.bank.BankLocation;
 import org.dreambot.api.methods.map.Tile;
+import java.util.Arrays;
 
 public class LocationManager {
     private static final Tile SHRIMP_LOCATION = new Tile();
@@ -38,7 +40,22 @@ public class LocationManager {
     }
 
     public static Tile decideWhereToBank(){
+        Tile[] allBankLocations = {BANK_SHRIMP_LOCATION, BANK_SARDINE_LOCATION, BANK_HERRING_LOCATION, BANK_TROUT_LOCATION, BANK_SALMON_LOCATION};
+        double[] distances = {0, 0, 0, 0, 0};
 
+        int indexCounter = 0;
+        for (Tile bankTile: allBankLocations){
+            distances[indexCounter++] = bankTile.distance();
+        }
+
+        int minIndex = 0; // Start with the assumption that the 0th element is the smallest
+        for (int i = 1; i < distances.length; i++) {
+            if (distances[i] < distances[minIndex]) {
+                minIndex = i;
+            }
+        }
+
+        return allBankLocations[minIndex];
     }
 
 
