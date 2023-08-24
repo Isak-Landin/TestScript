3ee2253fb16e9e8ae890005b55f4acdfbc85aa9f
Isak-Landin <utbisalan@gmail.com>
2023-08-24 22:40:02 +0200

    Update FishingManager.java

--- src/FishingManager.java
+++ src/FishingManager.java
@@ -1,9 +1,17 @@
+import org.dreambot.api.methods.interactive.Players;
+
 public class FishingManager {
-    public static double getClosestFishingSpot(){
+    private static final int animationForFishing = 1111;
+    public static double getClosestFishingSpotOnLocation(){
 
     }
 
     public static boolean isCurrentlyFishing(){
+        int myAnimation = Players.getLocal().getAnimation();
+        return myAnimation == animationForFishing;
+    }
+
+    public static boolean doIHaveAllToolsNeeded(TaskTypeUtility.FishingTask currentTask){
 
     }
 }
