a9e78bb98ee195bf464b79bafef4bd7b33fc6557
Isak-Landin <utbisalan@gmail.com>
2023-08-26 05:15:48 +0200

    Update TestScript.java

--- src/TestScript.java
+++ src/TestScript.java
@@ -1,14 +1,20 @@
 import org.dreambot.api.script.AbstractScript;
 import org.dreambot.api.script.Category;
 import org.dreambot.api.script.ScriptManifest;
+import org.dreambot.api.script.listener.GameTickListener;
 
 @ScriptManifest(name = "Re-testing", description = "Just testing setup before building anything", author = "Isak, Jacob",
         version = 1.0, category = Category.COOKING, image = "")
-public class TestScript extends AbstractScript {
+public class TestScript extends AbstractScript implements GameTickListener {
 
     @Override
     public int onLoop() {
         return 0;
+    }
+
+    @Override
+    public void onGameTick(){
+        FishingManager.isCurrentlyFishing();
     }
 
 }
