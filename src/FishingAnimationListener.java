5f7b8c5ede582626169b28ff903e353d42e2fcfd
Isak-Landin <utbisalan@gmail.com>
2023-08-26 05:15:29 +0200

    Create FishingAnimationListener.java

---
+++ src/FishingAnimationListener.java
@@ -0,0 +1,9 @@
+import org.dreambot.api.script.listener.AnimationListener;
+import org.dreambot.api.wrappers.interactive.Player;
+
+public class FishingAnimationListener implements AnimationListener {
+    @Override
+    public void onPlayerAnimation(Player player, int animation, int animationDelay) {
+        // Your implementation here
+    }
+}
