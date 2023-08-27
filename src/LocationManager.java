17b78f5b9eb67698d8e90caeb2975a80165066c0
Isak-Landin <utbisalan@gmail.com>
2023-08-27 05:33:28 +0200

    Update LocationManager.java

--- src/LocationManager.java
+++ src/LocationManager.java
@@ -4,6 +4,34 @@ import org.dreambot.api.methods.map.Tile;
 import java.util.Arrays;
 
 public class LocationManager {
+
+    /** Find out how to see if a fishing spot is x tiles away instead.*/
+    private static final double allowedDistanceFromShrimpTile = 15;
+    private static final double allowedDistanceFromSardineTile = 15;
+    private static final double allowedDistanceFromHerringTile = 15;
+    private static final double allowedDistanceFromSalmonAndTroutTile = 15;
+    private static final double allowedDistanceFromGeneralFishingSpot = 15;
+    public static final double allowedDistanceFromBank = 5;
+
+
+    public static double maximumAllowedDistanceForFishingTask(TaskTypeUtility.FishingTask myFishingTask){
+        switch (myFishingTask){
+            case SHRIMPS -> {
+                return allowedDistanceFromShrimpTile;
+            }
+            case SARDINE -> {
+                return allowedDistanceFromSardineTile;
+            }
+            case HERRING -> {
+                return allowedDistanceFromHerringTile;
+            }
+            case SALMONANDTROUT -> {
+                return allowedDistanceFromSalmonAndTroutTile;
+            }
+        }
+        return allowedDistanceFromGeneralFishingSpot;
+    }
+
     private static final Tile SHRIMP_LOCATION = new Tile();
     private static final Tile SARDINE_LOCATION = new Tile();
     private static final Tile HERRING_LOCATION = new Tile();
